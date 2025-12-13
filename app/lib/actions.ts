4f86550150b50b810055c6fc60c2a2ad9ca95088
Michael Lavina <michaellavina@macbookpro.mynetworksettings.com>
2025-12-13 15:46:43 -0500

    chapter 11 crud with server actions

---
+++ app/lib/actions.ts
@@ -0,0 +1,89 @@
+'use server';
+
+import {z} from 'zod';
+import {revalidatePath} from 'next/cache';
+import {redirect} from 'next/navigation';
+import postgres from 'postgres';
+import {Invoice} from '@/app/lib/definitions';
+
+const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'});
+
+export interface InvoiceFormData {
+  customerId: string;
+  amount: string; // in cents
+  status: Invoice['status'];
+}
+
+const InvoiceFormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+
+const CreateInvoice = InvoiceFormSchema.omit({id: true, date: true});
+
+export async function createInvoice(formData: FormData) {
+  const invoiceFormData: InvoiceFormData = {
+    customerId: formData.get('customerId') as string,
+    amount: formData.get('amount') as string,
+    status: formData.get('status') as Invoice['status'],
+  };
+
+  const {customerId, amount, status} = CreateInvoice.parse(invoiceFormData);
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  try {
+    await sql`INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to create invoice.');
+  }
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+// Use Zod to update the expected types
+const UpdateInvoice = InvoiceFormSchema.omit({date: true});
+
+export async function updateInvoice(formData: FormData) {
+  const {id, customerId, amount, status} = UpdateInvoice.parse({
+    id: formData.get('id'),
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+const DeleteInvoice = z.object({
+  id: z.string(),
+});
+
+export async function deleteInvoice(formData: FormData) {
+  const {id} = DeleteInvoice.parse({
+    id: formData.get('id'),
+  });
+
+  await sql`
+    DELETE FROM invoices
+    WHERE id = ${id}
+  `;
+
+  revalidatePath('/dashboard/invoices');
+}
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
+import {CustomerField, InvoiceForm} from '@/app/lib/definitions';
 import {
   CheckIcon,
   ClockIcon,
@@ -8,7 +8,8 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { Button } from '@/app/ui/button';
+import {Button} from '@/app/ui/button';
+import {updateInvoice} from '@/app/lib/actions';
 
 export default function EditInvoiceForm({
   invoice,
@@ -18,7 +19,8 @@ export default function EditInvoiceForm({
   customers: CustomerField[];
 }) {
   return (
-    <form>
+    <form action={updateInvoice}>
+      <input type="hidden" name="id" value={invoice.id} />
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -35,7 +37,7 @@ export default function EditInvoiceForm({
               <option value="" disabled>
                 Select a customer
               </option>
-              {customers.map((customer) => (
+              {customers.map(customer => (
                 <option key={customer.id} value={customer.id}>
                   {customer.name}
                 </option>
@@ -68,9 +70,7 @@ export default function EditInvoiceForm({
 
         {/* Invoice Status */}
         <fieldset>
-          <legend className="mb-2 block text-sm font-medium">
-            Set the invoice status
-          </legend>
+          <legend className="mb-2 block text-sm font-medium">Set the invoice status</legend>
           <div className="rounded-md border border-gray-200 bg-white px-[14px] py-3">
             <div className="flex gap-4">
               <div className="flex items-center">
