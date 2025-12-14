280ee925a795aca7bb1b3be8138143ca49c067fa
Michael Lavina <michaellavina@macbookpro.mynetworksettings.com>
2025-12-13 23:21:37 -0500

    chapter 14 auth

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,6 +5,8 @@ import {revalidatePath} from 'next/cache';
 import {redirect} from 'next/navigation';
 import postgres from 'postgres';
 import {Invoice} from '@/app/lib/definitions';
+import {signIn} from '@/auth';
+import {AuthError} from 'next-auth';
 
 const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'});
 
@@ -127,4 +129,20 @@ export async function deleteInvoice(formData: FormData) {
   }
 
   revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(prevState: string | undefined, formData: FormData) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
